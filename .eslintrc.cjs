b223100f28d29abc7025c01f0871f4806cc5fea4
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2024-02-18 23:06:25 +0100

    added some vue-related eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,9 @@
 module.exports = {
 	root: true,
 	extends: ['@nuxt/eslint-config', 'plugin:tailwindcss/recommended', 'prettier'],
+	rules: {
+		'vue/component-tags-order': ['error', { order: ['script', 'template', 'style'] }],
+		'vue/multi-word-component-names': 'warn',
+		'vue/padding-line-between-blocks': ['error', 'always'],
+	},
 };
