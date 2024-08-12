71d4f20f595fe2f9a18c49c7b42d030852c6a545
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2024-08-12 18:31:54 +0200

    updated dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	compatibilityDate: '2024-08-12',
 	css: ['~/assets/css/main.css'],
 	devtools: { enabled: true },
 	postcss: {
